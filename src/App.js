afc0b6f194f4c7e699f93f28996a66b003c3f761
OmkarParekh <omkarparekh600@gmail.com>
2020-03-14 14:48:22 +0530

    Intial the page

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./App.css";
+import Clanwar from "./Component/clanwar"
+
+export default class App extends Component {
+ 
+
+render(){
+ 
+ 
+  return(<div>{
+   <Clanwar/> 
+  }</div>
+  );
+}
+}
